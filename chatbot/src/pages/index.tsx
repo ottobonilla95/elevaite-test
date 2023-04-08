398100a734a6cc206af798abfdcbf0de17a5e2a5
Aadhithya Dinesh <aadhithyadinesh@Aadhithyas-MacBook-Pro.local>
2023-04-08 00:46:55 -0700

    Update UI Major v2

--- chatbot/src/pages/index.tsx
+++ chatbot/src/pages/index.tsx
@@ -1,18 +1,9 @@
 import { Inter } from "next/font/google";
-
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import TextareaAutosize from "@mui/base/TextareaAutosize";
-import { BsFillCursorFill } from "react-icons/bs";
-
-import Message from "./components/message";
 import Header from "./components/header";
-import CircularIndeterminate from "./components/progress_spinner";
-
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
-const inter = Inter({ subsets: ["latin"] });
+// import SideBar from "./components/sidebar";
+import { AiOutlinePlus } from "react-icons/ai";
+import ChatWindow from "./components/chatwindow";
+import { useEffect, useState } from "react";
 
 export type MessageDetails = {
   id: number;
@@ -21,101 +12,31 @@ export type MessageDetails = {
 };
 
 export default function Home() {
-  const [ask, setAsk] = useState<string>("");
-  const [messages, setMessages] = useState<MessageDetails[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [messageIdCount, setMessageIdCount] = useState(0);
-  const fetchAnswer = useRef(() => {});
+  const [chats, setChats] = useState<JSX.Element[]>([<ChatWindow key={0} />]);
+  const [count, setCount] = useState(1);
 
-  function handleClick(event: any) {
-    setIsLoading(() => true);
-    fetchAnswer.current();
+  const removeElement = (index: number) => {
+    const newChats = chats.filter((_, i) => i !== index);
+    setChats(() => newChats);
+  };
+
+  function handleClick() {
+    console.log("Clicked");
+    setCount(()=>count+1);
+    setChats(() => [<ChatWindow key={count} />]);
   }
-  function handleClearMessages() {
-    setMessages(() => []);
-  }
-
-  const handleKeyDown = (e: any) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      handleClick(e);
-    }
-  };
-
-  fetchAnswer.current = async () => {
-    return await axios
-      .get("https://api.iopex.ai/query", { params: { query: ask } })
-      .then((res) => {
-        console.log(ask);
-        console.log(res);
-        const answer =
-          "" +
-          res["data"]["text"] +
-          ` <p>Please checkout these urls for more information:</p>
-            <li><a target="_blank" href=${res["data"]["url_1"]}>${res["data"]["url_1"]}</a></li> 
-            <li><a target="_blank" href=${res["data"]["url_2"]}>${res["data"]["url_2"]}</a></li> 
-            <li><a target="_blank" href=${res["data"]["url_3"]}> ${res["data"]["url_3"]}</a></li>
-            `;
-
-        if (answer) {
-          setMessages(() => [
-            ...messages,
-            { id: messageIdCount, message: ask, from: "user" },
-            { id: messageIdCount + 1, message: answer, from: "system" },
-          ]);
-          setAsk(() => "");
-          setMessageIdCount(() => messageIdCount + 2);
-          setIsLoading(() => false);
-        }
-      })
-      .catch((error) => console.log("Error occured"));
-  };
-
-  const change = (event: any) => {
-    setAsk(() => event.target.value);
-  };
 
   return (
     <>
       <Header />
-      <div className="final-container">
-        <div className="band"></div>
-        <div className="parent-container">
-          <div className="parent">
-            {messages.map((message) => (
-              <>
-                <Message key={message.id} message={message} />
-              </>
-            ))}
-            <div className="container-body">
-              <TextareaAutosize
-                minRows={1}
-                maxRows={7}
-                className="chat-input"
-                onChange={change}
-                value={ask}
-                onKeyDown={handleKeyDown}
-                placeholder="Ask questions regarding your products here"
-              />
-              {isLoading ? (
-                <div className="loader">
-                  <CircularIndeterminate />
-                </div>
-              ) : (
-                <button className="button" onClick={handleClick}>
-                  <BsFillCursorFill size="30px" />
-                </button>
-              )}
-            </div>
-            <div>
-              {messages.length > 0 ? (
-                <button onClick={handleClearMessages} className="clear-button">
-                  Clear Messages
-                </button>
-              ) : null}
-            </div>
-          </div>
+      <div className="app-container">
+        <div className="sidebar-container">
+          <button onClick={handleClick} className="sidebar-button">
+            <AiOutlinePlus />
+            New Case
+          </button>
         </div>
+        {chats.map((chat, idx) => chat)}
       </div>
     </>
   );
