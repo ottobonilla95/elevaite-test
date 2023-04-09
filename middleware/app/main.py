9fc9bd9a922f2428cacb9af07d78788383cddc35
Binu Ramachandran <binu.ramachandran@shopkick.com>
2023-04-09 13:39:26 -0700

    Added threshold check before summarizing from OpenAI

--- middleware/app/main.py
+++ middleware/app/main.py
@@ -18,6 +18,8 @@ embedding_model = "text-embedding-ada-002"
 collection_name = "kbDocs_openAI"
 MODEL = "text-embedding-ada-002"
 OPEN_AI_KEY = "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"
+DEFAULT_RESPONSE = "Sorry, I am unable to answer your question. Can you please specify which product the query is for?"
+DEFAULT_THRESHOLD = 0.75
 
 qdrant_client = QdrantClient(
     url="https://a29a4a5f-b731-4ffa-befd-5c3f702c66f3.us-east-1-0.aws.cloud.qdrant.io:6333", 
@@ -52,8 +54,11 @@ def get_query_completion(query: str):
     for idx, result in enumerate(semantic_text_results):
         logger.info(f'Result {idx} - Text: {result.payload["Text"]}, Score: {result.score}')
     
-    query_completion = get_query_completion_result(query, semantic_text_results)
-    return get_response(query_completion, semantic_text_results)
+    if is_above_threshold(semantic_text_results) == True:
+        query_completion = get_query_completion_result(query, semantic_text_results)
+        return get_response(query_completion, semantic_text_results)
+    else:
+        return get_default_response()
 
 def get_semantic_search_results(vectorized_ask):
     return qdrant_client.search(
@@ -87,3 +92,11 @@ def get_response(query_completion, semantic_results: list):
         response_dict[response_key[0]] = response_val[0]
         response_dict[response_key[1]] = response_val[1]
     return response_dict
+
+def get_default_response():
+    return {"text" : DEFAULT_RESPONSE}
+
+def is_above_threshold(sementic_text_results):
+    is_above_threshold = False if len(sementic_text_results) <= 0 or sementic_text_results[0].score <= DEFAULT_THRESHOLD else True
+    logger.info(f'Results above threshold - {is_above_threshold}')
+    return is_above_threshold
