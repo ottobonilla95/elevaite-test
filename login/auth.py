563beba27d0f9be0ab2c1624808f8439cc02cbf8
Binu Ramachandran <binu.ramachandran@shopkick.com>
2023-05-23 19:20:12 -0700

    Add JWT token response

--- login/auth.py
+++ login/auth.py
@@ -13,6 +13,9 @@ from starlette.responses import JSONResponse
 from starlette.responses import RedirectResponse
 from starlette.responses import HTMLResponse
 
+import jwt
+from db.utils import Jwt_User_Claim
+
 # Create the auth app
 auth_app = FastAPI()
 logger = logging.getLogger("auth_app")
@@ -20,6 +23,7 @@ logger = logging.getLogger("auth_app")
 # OAuth settings
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or None
 if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
     raise BaseException('Missing Google Auth env variables')
 
@@ -35,7 +39,7 @@ oauth.register(
 
 # Set up the middleware to read the request session
 SECRET_KEY = os.environ.get('SECRET_KEY') or None
-if SECRET_KEY is None:
+if SECRET_KEY is None or JWT_SECRET_KEY is None:
     raise 'Missing SECRET_KEY'
 auth_app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
@@ -50,12 +54,9 @@ async def auth(request: Request):
         access_token = await oauth.google.authorize_access_token(request)
     except OAuthError:
         return RedirectResponse(url='/')
-    user_data = access_token['userinfo']
-    logger.info(user_data)
-    request.session['user'] = dict(user_data)
-    user = request.session.get('user')
-    name = user.get('name')
-    return HTMLResponse(f'<p>Hello {name}!{str(user)}</p><a href=/logout>Logout</a>')
+    token = Jwt_User_Claim.jwt_token_from_oauth(oauth_user_data=dict(user_data = access_token['userinfo']))
+    headers = {'Authorization' : 'Bearer ' + token}
+    return RedirectResponse(url='https://elevaite-cb.iopex.ai', headers=headers)
 
 @auth_app.route('/logout')
 async def logout(request: Request):
