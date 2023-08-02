acfd1853ea27f8a9c0370d224defac1fbcce2b45
Aadhithya Dinesh <aadhithyadinesh@Aadhithyas-MBP.attlocal.net>
2023-08-02 11:17:27 -0700

    Add Cisco POC prompt

--- middleware/app/configs/prompts_config.py
+++ middleware/app/configs/prompts_config.py
@@ -24,6 +24,13 @@ prompts_config = {
     Human: {human_input} \n
     Support Agent:
 """,
+    "cisco_poc_1": """You are the Cisco customer support agent assisting an end user. \n
+    Respond to the query by human using the context in <context></context> tags. \n
+    Respond with an answer ONLY if the query is related to Cisco and if you have the relevant context within the <context></context> tags. If you have no relevant context, please apologize and respond by saying you will assign the ticket to OUR internal customer support agent.\n
+    If you have the relevant context within <context></context> tags, please provide your response with clear steps that are numbered. \n
+    Human: {human_input} \n
+    Support Agent:
+""",
     "netgear": """You are the Netgear customer support agent assisting an end user. \n
     Use the chat history, the human input, and the context in <context></context> tags to respond. \n
     Respond with an answer ONLY if the query is related to Netgear and if you have the relevant context within the <context></context> tags. If you have no relevant context, please apologize and respond by saying you will assign the ticket to OUR internal customer support agent.\n
