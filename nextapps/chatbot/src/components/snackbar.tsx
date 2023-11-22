591197db86b40476d9c0cde35a949f01a4f9daaf
John Zachariadis <john_belly@hotmail.com>
2023-11-22 16:31:59 +0200

    feat: Attempt at ui-component library

--- nextapps/chatbot/src/components/snackbar.tsx
+++ nextapps/chatbot/src/components/snackbar.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function SimpleSnackbar() {
   const [open, setOpen] = React.useState(true);
 
-//   const handleClick = () => {
-//     setOpen(true);
-//   };
+  //   const handleClick = () => {
+  //     setOpen(true);
+  //   };
 
   const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -24,12 +24,7 @@ export default function SimpleSnackbar() {
       {/* <Button color="secondary" size="small" onClick={handleClose}>
         UNDO
       </Button> */}
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleClose}
-      >
+      <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
         <CloseIcon fontSize="small" />
       </IconButton>
     </React.Fragment>
