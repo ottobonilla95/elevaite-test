0d6a2c313b46f847b5d49210fd2b3ad8fc3354b3
John Zachariadis <john_belly@hotmail.com>
2023-12-05 19:31:36 +0200

    feat: Added colored border when jumping to element from search

---
+++ nextapps/elevaite/public/icons/index.ts
@@ -0,0 +1,23 @@
+import slackIcon from "./slack.svg";
+import onedriveIcon from "./oneDrive.svg";
+import mongoDBIcon from "./mongoDB.svg";
+import teamsIcon from "./teams.svg";
+import supportBot from "./supportBot.svg";
+import deckBuilder from "./deckBuilder.svg";
+import insights from "./insights.svg";
+import campaignBuilder from "./campaignBuilder.svg";
+
+export const ApplicationIcons = {
+  workbench: {
+    slack: { src: slackIcon.src, alt: "Slack" },
+    oneDrive: { src: onedriveIcon.src, alt: "Onedrive" },
+    mongoDB: { src: mongoDBIcon.src, alt: "MongoDB" },
+    teams: { src: teamsIcon, alt: "Teams" },
+  },
+  applications: {
+    supportBot: { src: supportBot.src, alt: "Support Bot" },
+    deckBuilder: { src: deckBuilder.src, alt: "Deck Builder" },
+    insights: { src: insights.src, alt: "ElevAIte Insights" },
+    campaignBuilder: { src: campaignBuilder.src, alt: "Campaign Builder" },
+  },
+};
