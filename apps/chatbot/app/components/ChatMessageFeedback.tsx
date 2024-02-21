498c26e89314fefd5c033d3513c947996b6bd972
John Zachariadis <john_belly@hotmail.com>
2024-02-21 20:40:02 +0200

    fix(chatbot): Fixed build errors

--- apps/chatbot/app/components/ChatMessageFeedback.tsx
+++ apps/chatbot/app/components/ChatMessageFeedback.tsx
@@ -4,9 +4,6 @@ import { useState } from "react";
 import type { ChatMessageFileObject } from "../lib/interfaces";
 import "./ChatMessageFeedback.scss";
 
-
-
-
 interface ChatMessageFeedbackProps {
     id: string;
 }
@@ -25,60 +22,71 @@ export function ChatMessageFeedback(props: ChatMessageFeedbackProps): JSX.Elemen
 
     function handleUpload(): void {
         console.log("Uploading?", files);
-        setFiles(current => [...current, {
-            id: `testId${current.length}${1}`,
-            filename: `Test_file_${current.length+1}.doc`,
-        }]);
+        setFiles((current) => [
+            ...current,
+            {
+                id: `testId${current.length}1`,
+                filename: `Test_file_${current.length + 1}.doc`,
+            },
+        ]);
     }
 
     function handleFileDeletion(id: string): void {
-        setFiles(current => current.filter(item => item.id !== id));
+        setFiles((current) => current.filter((item) => item.id !== id));
     }
-
-
 
     return (
         <div className="chat-message-feedback-container">
-
-            <div className={["feedback-files-container", files.length > 0 ? "open" : undefined].filter(Boolean).join(" ")}>
+            <div
+                className={["feedback-files-container", files.length > 0 ? "open" : undefined]
+                    .filter(Boolean)
+                    .join(" ")}
+            >
                 <div className="feedback-files-accordion">
                     <div className="feedback-files-contents">
-                        {files.map(file => <FeedbackFile key={file.id} {...file} onDelete={handleFileDeletion} />)}
+                        {files.map((file) => (
+                            <FeedbackFile key={file.id} {...file} onDelete={handleFileDeletion} />
+                        ))}
                     </div>
                 </div>
             </div>
-            
+
             <SimpleInput
-                wrapperClassName={["feedback-input-field", files.length > 0 ? "open" : undefined].filter(Boolean).join(" ")}
+                wrapperClassName={["feedback-input-field", files.length > 0 ? "open" : undefined]
+                    .filter(Boolean)
+                    .join(" ")}
                 value={feedbackText}
-                onChange={(string) => { setFeedbackText(string); }}
+                onChange={(string) => {
+                    setFeedbackText(string);
+                }}
                 onKeyDown={handleKeyDown}
                 placeholder="Add your feedback."
                 rightIcon={
                     <div className="feedback-buttons">
-                        <CommonButton onClick={handleUpload} >
-                            <ChatbotIcons.SVGUpload/>
+                        <CommonButton onClick={handleUpload}>
+                            <ChatbotIcons.SVGUpload />
                         </CommonButton>
-                        <CommonButton onClick={handleSubmit} >
-                            <ChatbotIcons.SVGSend/>
+                        <CommonButton onClick={handleSubmit}>
+                            <ChatbotIcons.SVGSend />
                         </CommonButton>
-                    </div> 
+                    </div>
                 }
             />
-
         </div>
     );
 }
 
-
-
-function FeedbackFile(props: ChatMessageFileObject & { onDelete: (id: string) => void; }): JSX.Element {
+function FeedbackFile(props: ChatMessageFileObject & { onDelete: (id: string) => void }): JSX.Element {
     return (
         <div className="feedback-file-container">
-            <ChatbotIcons.SVGDocument/>
+            <ChatbotIcons.SVGDocument />
             <span title={props.filename}>{props.filename}</span>
-            <CommonButton onClick={() => { props.onDelete(props.id); }}>
-                <ChatbotIcons.SVGClose/>
+            <CommonButton
+                onClick={() => {
+                    props.onDelete(props.id);
+                }}
+            >
+                <ChatbotIcons.SVGClose />
             </CommonButton>
         </div>
     );
