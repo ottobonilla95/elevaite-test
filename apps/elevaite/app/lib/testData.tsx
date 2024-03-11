817e2e8a12b02458547ef90d7236fbda9f747a05
Onyx <onyxgvr@gmail.com>
2024-03-11 18:18:48 +0200

    Obsolete test data removed

--- apps/elevaite/app/lib/testData.tsx
+++ apps/elevaite/app/lib/testData.tsx
@@ -40,67 +40,67 @@ export const testPipelineFlow = [
 
 
 
-export const TEST_INSTANCES: AppInstanceObject[] = [
-    {
-        creator: "Test User",
-        datasetId: "Dataset Id Test 1",
-        id: "Test Instance 1",
-        startTime: dayjs().subtract(3, "days").toISOString(),
-        status: AppInstanceStatus.RUNNING,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-        pipelineStepStatuses: [
-            {
-              step: "647427ef-2654-4585-8aaa-e03c66915c91",
-              status: PipelineStatus.IDLE,
-              startTime: "2024-02-10T19:30:38.000Z",
-              endTime: "2024-02-10T20:22:38.000Z"
-            }
-        ]
-    },
-    {
-        creator: "Random User 1",
-        datasetId: "Dataset Id Test 2",
-        id: "Test Instance 2",
-        startTime: dayjs().subtract(2, "days").subtract(7, "hours").toISOString(),
-        status: AppInstanceStatus.RUNNING,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-    },
-    {
-        creator: "Test User",
-        datasetId: "Dataset Id Test 3",
-        id: "Test Instance 3",
-        startTime: dayjs().subtract(2, "days").subtract(7, "hours").toISOString(),
-        endTime: dayjs().subtract(2, "days").subtract(5, "hours").subtract(25, "minutes").toISOString(),
-        status: AppInstanceStatus.COMPLETED,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-    },
-    {
-        creator: "Random User 1",
-        datasetId: "Dataset Id Test 4",
-        id: "Test Instance 4",
-        startTime: dayjs().subtract(2, "days").subtract(9, "hours").toISOString(),
-        endTime: dayjs().subtract(2, "days").subtract(8, "hours").subtract(12, "minutes").toISOString(),
-        status: AppInstanceStatus.COMPLETED,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-    },
-    {
-        creator: "Test User",
-        datasetId: "Dataset Id Test 5",
-        id: "Test Instance 5",
-        startTime: dayjs().toISOString(),
-        status: AppInstanceStatus.STARTING,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-    },
-    {
-        creator: "Test User",
-        datasetId: "Dataset Id Test 6",
-        id: "Test Instance 6",
-        startTime: dayjs().subtract(11, "hours").toISOString(),
-        endTime: dayjs().subtract(8, "hours").subtract(2, "minutes").toISOString(),
-        status: AppInstanceStatus.FAILED,
-        selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
-    },
-];
+// export const TEST_INSTANCES: AppInstanceObject[] = [
+//     {
+//         creator: "Test User",
+//         datasetId: "Dataset Id Test 1",
+//         id: "Test Instance 1",
+//         startTime: dayjs().subtract(3, "days").toISOString(),
+//         status: AppInstanceStatus.RUNNING,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//         pipelineStepStatuses: [
+//             {
+//               step: "647427ef-2654-4585-8aaa-e03c66915c91",
+//               status: PipelineStatus.IDLE,
+//               startTime: "2024-02-10T19:30:38.000Z",
+//               endTime: "2024-02-10T20:22:38.000Z"
+//             }
+//         ]
+//     },
+//     {
+//         creator: "Random User 1",
+//         datasetId: "Dataset Id Test 2",
+//         id: "Test Instance 2",
+//         startTime: dayjs().subtract(2, "days").subtract(7, "hours").toISOString(),
+//         status: AppInstanceStatus.RUNNING,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//     },
+//     {
+//         creator: "Test User",
+//         datasetId: "Dataset Id Test 3",
+//         id: "Test Instance 3",
+//         startTime: dayjs().subtract(2, "days").subtract(7, "hours").toISOString(),
+//         endTime: dayjs().subtract(2, "days").subtract(5, "hours").subtract(25, "minutes").toISOString(),
+//         status: AppInstanceStatus.COMPLETED,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//     },
+//     {
+//         creator: "Random User 1",
+//         datasetId: "Dataset Id Test 4",
+//         id: "Test Instance 4",
+//         startTime: dayjs().subtract(2, "days").subtract(9, "hours").toISOString(),
+//         endTime: dayjs().subtract(2, "days").subtract(8, "hours").subtract(12, "minutes").toISOString(),
+//         status: AppInstanceStatus.COMPLETED,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//     },
+//     {
+//         creator: "Test User",
+//         datasetId: "Dataset Id Test 5",
+//         id: "Test Instance 5",
+//         startTime: dayjs().toISOString(),
+//         status: AppInstanceStatus.STARTING,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//     },
+//     {
+//         creator: "Test User",
+//         datasetId: "Dataset Id Test 6",
+//         id: "Test Instance 6",
+//         startTime: dayjs().subtract(11, "hours").toISOString(),
+//         endTime: dayjs().subtract(8, "hours").subtract(2, "minutes").toISOString(),
+//         status: AppInstanceStatus.FAILED,
+//         selectedPipeline: "8470d675-6752-4446-8f07-fe7a99949e42",
+//     },
+// ];
 
 
 
