e3a84f2548ad28f27dacebac959224be6722f122
John Zachariadis <johnbelly95@gmail.com>
2024-11-06 16:27:44 +0200

    refactor: Initial SSR refactor

--- apps/contracts/app/layout.tsx
+++ apps/contracts/app/layout.tsx
@@ -1,57 +1,52 @@
 import { Inter } from "next/font/google";
 import "./layout.scss";
-import "./layout.css"
-import Providers from "./ui/Providers";
+import "./layout.css";
+import Providers from "../ui/Providers";
 import { Metadata } from "next";
 import { NavBar } from "@repo/ui/components";
 
-
 const font = Inter({ subsets: ["latin"] });
 
-
 interface RootLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const breadcrumbLabels: Record<string, { label: string; link: string }> = {
   home: {
     label: "Applications",
     link: "/",
-  }
+  },
 };
 
 export const metadata: Metadata = {
-    title: "Contract Co-Pilot",
-    description: "Quickly discover and evaluate important data in your contracts and invoices.",
+  title: "Contract Co-Pilot",
+  description:
+    "Quickly discover and evaluate important data in your contracts and invoices.",
 };
 
-export default function RootLayout({children}: RootLayoutProps): JSX.Element {
-
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
   async function handleSearchInput(term: string): Promise<void> {
-    "use server"
+    "use server";
   }
 
   async function logOut(): Promise<void> {
-    "use server"
+    "use server";
   }
 
-    return (
-        <html lang="en">
-            <body className={font.className + " elevaite-main-container"}>
-                <Providers>
-
-                  <NavBar
-                    breadcrumbLabels={breadcrumbLabels}
-                    handleSearchInput={handleSearchInput}
-                    logOut={logOut}
-                    searchResults={[]}
-                    user={undefined}
-                  />
-                  <div className="children-container">
-                    {children}
-                  </div>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={font.className + " elevaite-main-container"}>
+        <Providers>
+          <NavBar
+            breadcrumbLabels={breadcrumbLabels}
+            handleSearchInput={handleSearchInput}
+            logOut={logOut}
+            searchResults={[]}
+            user={undefined}
+          />
+          <div className="children-container">{children}</div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
