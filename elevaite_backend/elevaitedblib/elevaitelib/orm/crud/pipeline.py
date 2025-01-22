ce018ef6b5ddac78df24a6e175dcddbcff1a68a1
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-01-22 18:25:09 +0200

    elevaitedlib : Update pipeline crud to reflect new models

--- elevaite_backend/elevaitedblib/elevaitelib/orm/crud/pipeline.py
+++ elevaite_backend/elevaitedblib/elevaitelib/orm/crud/pipeline.py
@@ -7,9 +7,39 @@ def get_pipelines(db: Session, skip: int = 0, limit: int = 0) -> list[models.Pip
     return db.query(models.Pipeline).offset(skip).limit(limit).all()
 
 
-def get_pipelines_of_application(db: Session, application_id: int, skip: int = 0, limit: int = 100) -> list[models.Pipeline]:
-    return db.query(models.Pipeline).filter(models.Pipeline.applicationId == application_id).offset(skip).limit(limit).all()
+def get_pipelines_of_project(
+    db: Session,
+    filter_function: Callable[[Query], Query],
+    project_id: UUID,
+    skip: int = 0,
+    limit: int = 0,
+) -> list[models.Pipeline]:
+    query = db.query(models.Pipeline)
+
+    if filter_function is not None:  # uncomment this when using validator
+        query = filter_function(query)
+    return query.filter(models.Pipeline.projects.any(id=project_id)).offset(skip).limit(limit).all()
 
 
-def get_pipeline_by_id(db: Session, pipeline_id: str) -> models.Pipeline:
-    return db.query(models.Pipeline).filter(models.Pipeline.id == pipeline_id).first()
+def get_pipeline_by_id(
+    db: Session,
+    pipeline_id: UUID,
+    filter_function: Callable[[Query], Query] | None = None,
+) -> models.Pipeline:
+    query = db.query(models.Pipeline)
+
+    if filter_function is not None:  # uncomment this when using validator
+        query = filter_function(query)
+    return query.filter(models.Pipeline.id == pipeline_id).first()
+
+
+def create_pipeline(db: Session, pipeline_dto: PipelineCreate) -> models.Pipeline:
+    _pipeline = models.Pipeline(
+        name=pipeline_dto.name,
+        description=pipeline_dto.description,
+        entrypoint=pipeline_dto.entrypoint,
+    )
+    db.add(_pipeline)
+    db.commit()
+    db.refresh(_pipeline)
+    return _pipeline
