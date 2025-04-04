e2ab6a8cd640e8bb9922722aa8ba0b13adb99869
John Zachariadis <johnbelly95@gmail.com>
2025-04-04 15:08:19 +0300

    feat: Even more moving of directories

--- middleware/app/configs/collections_config.py
+++
@@ -1,8 +0,0 @@
-collections_config = {
-  "pan":"kbDocs_30Incidents_v2",
-  "cisco":"cisco_webex_all",
-  "cisco_clo":"cisco_clo_poc",
-  "servicenow":"servicenow-1536",
-  "bgpinsights":"bgp_logs_kb",
-  "alswitchlogs":"al_switch_logs"
-}
--- middleware/app/utils/get_tokens_count.py
+++
@@ -1,7 +0,0 @@
-import tiktoken
-
-## Count the tokens for given string
-def get_tokens_count(query: str):
-  encoding = tiktoken.encoding_for_model("text-davinci-003")
-  tokenSize=encoding.encode(query)
-  return(len(tokenSize))
