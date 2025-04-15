6cff5f25943db8bb401d52641d571b3308aef3cb
Nikhitha Kandula <nikhitha.kandula@iopex.com>
2025-04-14 20:54:43 -0700

    Changes for toshiba web app - demo

--- apps/toshiba/app/page.tsx
+++ apps/toshiba/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { MainAreaSwitcher } from "./components/advanced/MainAreaSwitcher";
 import { ProjectSidebar } from "./components/advanced/ProjectSidebar";
 import "./page.scss";
@@ -6,13 +9,19 @@ import "./page.scss";
 
 
 export default function Chatbot(): JSX.Element {
+  const [isExpanded, setIsExpanded] = useState(false)
 
 
   return (
-    <main className="chatbot-advanced-container">
+    <main className={`chatbot-advanced-container ${isExpanded ? "sidebar-expanded" : "sidebar-collapsed"}`}>
       {/* <Sidebar/> */}
-      <ProjectSidebar/>
-      <MainAreaSwitcher/>
+      <ProjectSidebar
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+      />
+      <MainAreaSwitcher
+        isSidebarCollapsed={isExpanded}
+      />
     </main>
   );
 }
