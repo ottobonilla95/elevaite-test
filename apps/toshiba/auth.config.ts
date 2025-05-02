6a607051d22e38796cf8ed8077eea94667ce9899
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-05-02 21:34:25 +0300

    toshiba ui : redirect user to the correct homepage and ensure redirect when trying to access /login and signed in

--- apps/toshiba/auth.config.ts
+++ apps/toshiba/auth.config.ts
@@ -3,10 +3,20 @@ import { stockConfig } from "@repo/lib";
 
 export const authConfig = {
   session: { strategy: "jwt", maxAge: 3600 },
+  pages: {
+    signIn: "/login",
+  },
   callbacks: {
-    authorized({ auth, request: { nextUrl: _nextUrl } }) {
+    authorized({ auth, request: { nextUrl } }) {
       // if (process.env.NODE_ENV === "development") return true;
       const isLoggedIn = Boolean(auth?.user);
+
+      // If the user is already logged in and trying to access the login page,
+      // redirect them to the home page
+      if (isLoggedIn && nextUrl.pathname === "/login") {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
       if (isLoggedIn) return true;
       return false; // Redirect unauthenticated users to login page
     },
