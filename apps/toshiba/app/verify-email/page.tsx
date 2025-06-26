e29c3948322e97cdbb2ac7584807a241e58fd3d2
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-06-26 16:56:11 +0000

    toshiba : Adjust env variable in verify-email

--- apps/toshiba/app/verify-email/page.tsx
+++ apps/toshiba/app/verify-email/page.tsx
@@ -32,7 +32,7 @@ export default function VerifyEmailPage() {
 
   const verifyEmail = async (token: string) => {
     try {
-      const backendUrl = process.env.AUTH_API_URL;
+      const backendUrl = process.env.NEXT_PUBLIC_AUTH_API_URL;
       const tenantId = process.env.NEXT_PUBLIC_AUTH_TENANT_ID ?? "default";
 
       if (!backendUrl) {
