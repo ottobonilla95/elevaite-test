aaec4b3cb773c8f88310ba1cc73ac7a3c9b3c73a
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-06-26 20:23:11 +0000

    toshiba : Fix top level session

--- apps/toshiba/app/chatbot/layout.tsx
+++ apps/toshiba/app/chatbot/layout.tsx
@@ -1,13 +1,13 @@
 import { ColorContextProvider } from "@repo/ui/contexts";
 import { ChatContextProvider } from "../ui/contexts/ChatContext";
 import { AppLayout } from "../components/AppLayout";
-
 import { SessionProvider } from "next-auth/react";
+import { auth } from "../../auth";
 
-// eslint-disable-next-line @typescript-eslint/require-await -- pretty sure this has to be async
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>): Promise<JSX.Element> {
+  const session = await auth();
   const breadcrumbs: Record<string, { label: string; link: string }> = {
     home: {
       label: "Ask Toshiba",
@@ -16,9 +16,9 @@ export default async function RootLayout({
   };
 
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <ColorContextProvider>
-        <ChatContextProvider>
+        <ChatContextProvider session={session}>
           <AppLayout breadcrumbs={breadcrumbs}>{children}</AppLayout>
         </ChatContextProvider>
       </ColorContextProvider>
