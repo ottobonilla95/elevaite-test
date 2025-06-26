dc2e14a4200cf1790e997d7e1a9aef703344dbfa
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-06-26 14:35:20 +0000

    toshiba : Fix navbar in /access

--- apps/toshiba/app/access/layout.tsx
+++ apps/toshiba/app/access/layout.tsx
@@ -1,50 +1,7 @@
-import { ElevaiteIcons, type SidebarIconObject } from "@repo/ui/components";
-import { ColorContextProvider } from "@repo/ui/contexts";
-import { RolesContextProvider } from "../lib/contexts/RolesContext";
-import { ClientAppLayout } from "../ui/ClientAppLayout";
-import { SessionProvider } from "next-auth/react";
-
-const breadcrumbLabels: Record<string, { label: string; link: string }> = {
-  access: {
-    label: "Access Management",
-    link: "/access",
-  },
-  home: {
-    label: "Applications",
-    link: "/",
-  },
-};
-
-const sidebarIcons: SidebarIconObject[] = [
-  {
-    icon: <ElevaiteIcons.SVGAccess />,
-    link: "/access",
-    description: "Access Management",
-  },
-  {
-    icon: <ElevaiteIcons.SVGApplications />,
-    link: "/",
-    description: "Applications",
-  },
-];
-
 export default function AccessLayout({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  return (
-    <SessionProvider>
-      <RolesContextProvider>
-        <ColorContextProvider>
-          <ClientAppLayout
-            breadcrumbLabels={breadcrumbLabels}
-            sidebarIcons={sidebarIcons}
-          >
-            {children}
-          </ClientAppLayout>
-        </ColorContextProvider>
-      </RolesContextProvider>
-    </SessionProvider>
-  );
+  return <>{children}</>;
 }
